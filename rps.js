14e77a01965420145066a53cbe8bbe580981021e
Zhi Rong <zhirong1109@hotmail.com>
2022-10-26 11:36:47 +0800

    Add rps.js

---
+++ rps.js
@@ -0,0 +1,62 @@
+let user1 = 0;
+let user2 = 0;
+
+const getComputerChoice = () => {
+    const randomNumber = (Math. floor(Math. random() * 3)) 
+    if (randomNumber === 0){ 
+        return 'Rock'; 
+    } else if (randomNumber === 1) {
+        return 'Paper'; 
+    } else if (randomNumber === 2) {
+        return 'Scissor'; 
+    } 
+} 
+
+const computerSelection = getComputerChoice();
+
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection == computerSelection) {
+        return 'Draw!'
+    } else if (playerSelection == 'Scissor' && computerSelection == 'Rock') {
+        user2 += 1;
+        return ('You Lose!');
+    } else if (playerSelection == 'Scissor' && computerSelection == 'Paper') {
+        user1 += 1;
+        return ('You Win!');
+    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        user2 += 1;
+        return ('You Lose!');
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissor') {
+        user1 += 1;
+        return ('You Win!');
+    } else if (playerSelection == 'Paper' && computerSelection == 'Scissor') {
+        user2 += 1;
+        return ('You Lose!');
+    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        user1 += 1;
+        return ('You Win!');
+    } else {
+        return 'Error'
+    }
+}
+
+let game = () => {
+    for (let i = 0; i < 5; i++) {
+        let playerInput = prompt('Rock, Paper or Scissor?')
+        const playerSelection = playerInput[0].toUpperCase() + playerInput.slice(1).toLowerCase();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(playerSelection);
+        console.log(computerSelection);
+        console.log(user1);
+        console.log(user2);
+    }
+    if (user1 > user2) {
+        console.log('Hurray!')
+    } else if (user2 > user1) {
+        console.log('Boo!')
+    } else {
+        console.log('what?')
+    }
+}
+
+game();
